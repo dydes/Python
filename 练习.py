8df91292862ff701db50b7ecef9584b556c11444
dydes <942238839@qq.com>
2021-04-28 15:54:35 +0800

    1

---
+++ 练习.py
@@ -0,0 +1,12 @@
+#引入包
+import xlwings as xw
+
+app = xw.App(visible=True, add_book=False)
+app.display_alerts = True    # 关闭一些提示信息，可以加快运行速度。 默认为 True。
+app.screen_updating = True    # 更新显示工作表的内容。默认为 True。关闭它也可以提升运行速度。
+
+# 新建工作簿。
+wb = app.books.add()
+
+# 获取当前活动工作表
+sht = wb.sheets.active
