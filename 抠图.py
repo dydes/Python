527ccd10a8abe6b2cd01b5e0f91d2e892cc1465d
dydes <942238839@qq.com>
2021-04-28 16:20:25 +0800

    111

--- 抠图.py
+++ 抠图.py
@@ -2,7 +2,7 @@ pip install removebg
 from removebg import RemoveBg
 
 #获取API
-rmbg = RemoveBg("xrCqrD4fee1fGhej3yzDGAmc", "error.log") 
+rmbg = RemoveBg("xrCqrD4fee1fGhej3yzDGAmc", "error.log")       
 
 #图片地址
 rmbg.remove_background_from_img_file("D:\会通\9.没事PS\timg.jpg")
